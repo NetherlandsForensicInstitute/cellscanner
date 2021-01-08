fb479045aed9eb03443773643754cf9bf487531b
niels <nprins@pop-os.localdomain>
2021-01-08 09:39:04 +0100

    recordingsate setter

--- app/src/main/java/cellscanner/wowtor/github/com/cellscanner/recorder/Recorder.java
+++ app/src/main/java/cellscanner/wowtor/github/com/cellscanner/recorder/Recorder.java
@@ -1,13 +1,24 @@
 package cellscanner.wowtor.github.com.cellscanner.recorder;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 
 public class Recorder {
+    final private static String PREFS_NAME = "MY_PREFS_NAME";
+
     public static boolean inRecordingState(Context context) {
         // TODO: PICK A NAME NOT `MY_PREFS_NAME`
         return context
-                .getSharedPreferences("MY_PREFS_NAME", Context.MODE_PRIVATE)
+                .getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
                 .getBoolean("RECORDING", false);
     }
+
+    public static void setRecordingState(Context context, Boolean state) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("RECORDING", state);
+        editor.apply();
+    }
+
 }
