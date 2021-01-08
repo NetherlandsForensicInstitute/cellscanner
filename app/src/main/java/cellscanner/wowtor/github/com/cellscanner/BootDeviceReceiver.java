80cc99b7a1b40d014270bf5091bae1d5c3a191f3
niels <nprins@pop-os.localdomain>
2021-01-08 10:05:05 +0100

    check is recording field on boot

--- app/src/main/java/cellscanner/wowtor/github/com/cellscanner/BootDeviceReceiver.java
+++ app/src/main/java/cellscanner/wowtor/github/com/cellscanner/BootDeviceReceiver.java
@@ -3,8 +3,9 @@ package cellscanner.wowtor.github.com.cellscanner;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.Toast;
+
+import cellscanner.wowtor.github.com.cellscanner.recorder.Recorder;
 
 public class BootDeviceReceiver extends BroadcastReceiver {
 
@@ -14,11 +15,13 @@ public class BootDeviceReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         String action = intent.getAction();
-
         String message = App.TITLE + " on boot action is " + action;
-
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
 
-        LocationService.start(context);
+        // TODO: @ check if the right application is sending the request
+        if (Recorder.inRecordingState(context)){
+            // TODO: make startup version aware
+            LocationService.start(context);
+        }
     }
 }
