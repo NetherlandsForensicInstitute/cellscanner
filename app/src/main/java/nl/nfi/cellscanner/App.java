24dad3f2089650e4c499f9b1f054e7d19d095aa6
niels <nprins@pop-os.localdomain>
2021-01-12 11:50:14 +0100

    renamed getDataPath => getDataFile

--- app/src/main/java/nl/nfi/cellscanner/App.java
+++ app/src/main/java/nl/nfi/cellscanner/App.java
@@ -36,7 +36,7 @@ public class App extends Application {
         // close the existing database connection
         dbhelper.close();
         // TODO: why not: appcontext.deleteDatabase(Database.getDataPath(appcontext).getName());?
-        File path = Database.getDataPath(appcontext);
+        File path = Database.getDataFile(appcontext);
         path.delete();
         dbhelper = new OpenHelper(appcontext);
     }
@@ -49,7 +49,7 @@ public class App extends Application {
      */
     private static class OpenHelper extends SQLiteOpenHelper {
         public OpenHelper(Context context) {
-            super(context, Database.getDataPath(context).toString(), null, DATABASE_VERSION);
+            super(context, Database.getDataFile(context).toString(), null, DATABASE_VERSION);
         }
 
         @Override
