f173e9b9fbfb03211a63660fe4de87f26a4e212f
nprins <niels.prins@gmail.com>
2021-02-02 16:18:45 +0100

    intergrating upload worker

--- app/src/main/java/nl/nfi/cellscanner/UserDataUploadWorker.java
+++ app/src/main/java/nl/nfi/cellscanner/UserDataUploadWorker.java
@@ -11,9 +11,20 @@ import androidx.core.app.NotificationCompat;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.FileInputStream;
+import java.util.concurrent.TimeUnit;
+
 
 public class UserDataUploadWorker extends Worker {
     public static final String TAG = UserDataUploadWorker.class.getSimpleName();
+
+    private static final String HOSTNAME = "192.168.2.29";
+    private static final String USERNAME = "myuser";
+    private static final String MYPASS = "mypass";
+
 
     public UserDataUploadWorker(
             @NonNull Context context,
@@ -27,12 +38,52 @@ public class UserDataUploadWorker extends Worker {
     public Result doWork() {
         Log.i(TAG, "I will do my work!!!");
 
+        FileInputStream fileInputStream;
+        FTPClient con = new FTPClient();
+
+
+        try
+        {
+            con.connect(HOSTNAME);
+            if (con.login(USERNAME, MYPASS))
+            {
+                con.enterLocalPassiveMode(); // important!
+                con.setFileType(FTP.BINARY_FILE_TYPE);
+
+                // get the file and send it. A
+                fileInputStream = new FileInputStream(Database.getDataFile(getApplicationContext()));
+                boolean result = con.storeFile(getFileName("deviceID", ""), fileInputStream);
+                TimeUnit.SECONDS.sleep(15);
+                fileInputStream.close();
+
+                if (result) {
+                    Log.i(TAG, "upload result: succeeded");
+
+                    /*when file has been uploaded, the old data can be flushed*/
+
+                } else {
+                    Log.i(TAG, "upload result: Failed");
+                }
+
+                // disconnect from the server
+                con.logout();
+                con.disconnect();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
 
 
         return Result.success();
     }
 
+
+    private String getFileName(String deviceId, String timeStamp) {
+        return String.format("s{0}.sqlite", deviceId);
+    }
 
     /* temp, this should be removed */
     private void showNotification(String task, String desc) {
