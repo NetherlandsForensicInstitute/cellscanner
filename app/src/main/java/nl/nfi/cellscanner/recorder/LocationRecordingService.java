c52f8d1a98149e95e05e8406daf85fad5e1cf968
wb <w.bosma@nfi.nl>
2021-03-19 09:44:55 +0100

    fix notification channel creation time

--- app/src/main/java/nl/nfi/cellscanner/recorder/LocationRecordingService.java
+++ app/src/main/java/nl/nfi/cellscanner/recorder/LocationRecordingService.java
@@ -69,6 +69,7 @@ public class LocationRecordingService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        createNotificationChannel();
         startForeground(STATUS_NOTIFICATION_ID, getActivityNotification("started"));
 
         /* construct required constants */
@@ -77,7 +78,6 @@ public class LocationRecordingService extends Service {
         mDB = CellScannerApp.getDatabase();
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        createNotificationChannel();
 
         /* store some constants in the database */
         mDB.storeInstallID(getApplicationContext());
